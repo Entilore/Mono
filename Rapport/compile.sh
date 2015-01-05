d6a99fd429c1995a86634ac09acec9dd6a2fd98d
Thomas Hareau <thomashareau@free.fr>
2015-01-05 20:42:06 +0100

    modification pour le glossaire

--- Rapport/compile.sh
+++ Rapport/compile.sh
@@ -1,6 +1,5 @@
 pdflatex -synctex=1 -interaction=nonstopmode main.tex
 biber main
-makeglossaries main
 pdflatex -synctex=1 -interaction=nonstopmode main.tex
 pdflatex -synctex=1 -interaction=nonstopmode main.tex
 
