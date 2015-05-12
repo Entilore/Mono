7804cea4ea274297f2f99d6df9d2e0d0e47cfcc7
Jean Chorin <jchorin@insa-rennes.fr>
2015-05-12 14:34:57 +0200

    imprimante.php added

---
+++ Website/imprimante.php
@@ -0,0 +1,46 @@
+<?php  include("header_navbar.php");       ?>
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+      <h2>L'exemple des imprimantes : </h2>
+      <p class="lead">Le fait le plus troublant d’obsolescence programmée présumée est très certainement celui des imprimantes. Alexandre Delaigue est un économiste réfutant la thèse de l’obsolescence programmée et considère lui même ce cas comme « un exemple curieux ».  En effet, il parait compliqué d’affirmer que le constructeur n’ait pas souhaité limiter la durée de vie de son produit.
+     </p>
+    
+      <div class=row>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/Ford-T.jpg">
+              <figcaption>Un des premiers modèles de Ford T</figcaption>
+        </div>
+    
+        <div class="col-md-9">
+          <p class="lead">L’exemple a été, comme beaucoup d’autres, mis en évidence par le reportage d’Arte : Prêt à jeter. Y est présenté un informaticien de Barcelone dont l’imprimante ne marche plus. Refusant d’acheter une nouvelle machine, l’espagnol se renseigne sur internet, et trouve la raison de sa panne : une puce électronique bloque l’appareil. Il s’agirait en fait d’un compteur, qui empêche l’engin d’imprimer après un certain nombre d’impressions. Si cette théorie est véridique, il devrait donc être possible de réinitialiser le compteur ?
+
+
+           </p>
+        </div>
+      </div>
+      
+      <div class=row>
+        <div class="col-md-9">
+          <p class="lead">C’est ce qu’a réussi à faire un Russe #NOM#. Il a créé et rendu public un logiciel permettant de remettre à zéro la pièce responsable de la panne de la machine. Et effectivement, l’imprimante devient de nouveau fonctionnelle après la manipulation.
+          </p>
+        </div>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
+              <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+            L’existence d’une puce dans l’appareil et qui en bloque son usage est donc prouvée. Ceci ressemble fortement à une volonté du constructeur de réduire la vie du produit. De plus, le service après-vente consulté par l’informaticien espagnol est affligeant : on lui conseille de changer de machine, le coût de la réparation étant supérieur à celui d’une imprimante neuve.
+          </p>
+        </div>
+      </div>
+    
+    </div><!-- /.container -->
+
+
+<?php  include("footer.php");       ?>
