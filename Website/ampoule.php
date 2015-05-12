583ca3a5e83cda1982504b4b85bdf996da4dcf93
Jean Chorin <jchorin@insa-rennes.fr>
2015-05-12 22:48:32 +0200

    ampoule.php added

---
+++ Website/ampoule.php
@@ -0,0 +1,60 @@
+<?php  include("header_navbar.php");       ?>
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+      <h2>L'ampoule de Livermore </h2>
+      <p class="lead">L’ampoule est l’un des premiers exemples de l’obsolescence programmée. Apparu suite au cartel Phoebus, où les 
+        grands constructeurs d’ampoules, l’américain General Electric, l’allemand Osram, Philips et d’autres, se sont mis d’accord
+         pour réduire la durée de vie des lampes à filament.
+     </p>
+    
+      <div class=row>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/Ford-T.jpg">
+              <figcaption>Un des premiers modèles de Ford T</figcaption>
+        </div>
+    
+        <div class="col-md-9">
+          <p class="lead">Avant le cartel, les industries produisaient des lampes avec une durée de vie allant  jusqu’à 2300 heures 
+            en moyenne.  Ils ont ensuite décidé que la durée de vie des ampoules ne devrait pas dépasser 1000h. D’après 
+            plusieurs chercheurs, le cartel avait pour but de limiter la durée de vie des lampes et  de pousser les gens à acheter plus 
+            de lampes. Il y a aussi une étude qui a montré que le filament de fer des ampoules de nos jours est plus fragile que sur les 
+            anciennes ampoules. 
+
+
+           </p>
+        </div>
+      </div>
+      
+      <div class=row>
+        <div class="col-md-9">
+          <p class="lead">Pourtant, l’ampoule de Livermore est un exemple des lampes qui ont dépassé les 1000 heures. La vieille ampoule, 
+            installée aux Etats-Unis en 1901 dans une caserne des pompiers, est allumée sans interruption depuis 114 ans. Mais pourquoi 
+            fonctionne-t-elle toujours ? C’est grâce à sa faible luminosité et sa basse consommation. Cette ampoule était concue pour 
+            consommer 60 watt, mais aujourd’hui, elle a une puissance de 4 watt. 
+          </p>
+        </div>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
+              <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+            Des études ont montré que l’ampoule de Livermore ne produit que 0,3 % de sa luminosité originale. Or moins la lampe est 
+            lumineuse, plus sa durée de vie est importante.
+          </p>
+          <p class="lead">
+            Aujourd’hui, avec le progrès technologique, on trouve des lampes plus durables comme les lampes à diode electroluminescente 
+            ou LED en anglais, une ampoule à la fois durable et à basse consommation.
+          </p>
+        </div>
+      </div>
+    
+    </div><!-- /.container -->
+
+
+<?php  include("footer.php");       ?>
