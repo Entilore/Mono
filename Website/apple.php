ced3126cd7946f6378f289dc96d2241841ff47f6
Jean Chorin <jchorin@insa-rennes.fr>
2015-05-12 14:56:56 +0200

    apple.php added

---
+++ Website/apple.php
@@ -0,0 +1,65 @@
+<?php  include("header_navbar.php");       ?>
+
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+      <h2>Le cas Apple : </h2>
+      <p class="lead">La société Apple est régulièrement critiquée pour ses pratiques jugées douteuses. En effet Apple est un habitué des "erreurs" de conception et est souvent accusé d'obsolescence programmée.
+      </p>
+    
+      <div class=row>
+        <div class="col-md-2">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/Ford-T.jpg">
+              <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+    
+        <div class="col-md-10">
+          <p class="lead">Le premier exemple est l'iPod, dont la batterie n'était pas remplaçable pour les 1ère, 2ème et 3ème générations. Un client Apple qui voyait la batterie de son iPod inutilisable devait racheter un baladeur neuf. A la suite d’un procès intenté par Elizabeth Pritzyker, Apple met finalement en place une politique de remplacement des batteries.
+
+           </p>
+        </div>
+      </div>
+      
+      <div class=row>
+        <div class="col-md-10">
+          <p class="lead">Malgré la norme USB imposée aux fabricants de smartphones par l’Union Européenne, Apple a toujours utilisé ses propres connecteurs, tout en changeant la forme du connecteur d’une génération à une autre, exigeant le rachat des adaptateurs et des accessoires.
+          </p>
+        </div>
+        <div class="col-md-2">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
+              <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+          	Le phénomène “Iphone slow” est également troublant. En effet ceci correspond à l’étude de graphes représentant le nombre de recherches Google sur les lenteurs de son téléphone en fonction de la marque de ce dernier. On remarque que les pics de recherches correspondent chez Apple à la sortie du nouvel OS, destiné à la génération suivante d’iPhone. Certains parlent d’Obsolescence logicielle (intentionnelle), car en effet on remarque l’absence de pics pour les téléphones Samsung.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6">
+          <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
+          <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+        <div class="col-md-6">
+          <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
+          <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+        </div>
+      </div>
+
+	  <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+          	Il faut cependant prendre ces exemples avec des du recul. Tout d’abord Samsung ne gère pas lui même les mises à jour des systèmes android. Mais Samsung est elle aussi une entreprise montrée du doigt sur des cas présumé d’obsolescence programmée (cas des condensateurs de certaines télés). Il faut prendre en compte que l’innovation est cause d’obsolescence, et que l’implémentation de technologies nouvelles augmente le risque d’erreur d'ingénierie. Or Samsung et Apple font partie des entreprises les plus innovantes au monde.
+          </p>
+        </div>
+      </div>
+    
+    </div><!-- /.container -->
+
+
+<?php  include("footer.php");       ?>
