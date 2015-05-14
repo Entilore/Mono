50ad043e9b2a830d67a67954cf72fd2beb307e1e
masoumiaa <aminesoumiaa@gmail.com>
2015-05-14 15:31:34 +0200

    page loi.php

---
+++ Website/loi.php
@@ -0,0 +1,42 @@
+<?php  include("header_navbar.php");       ?>
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+      <h2>Loi sur la transition énergétique</h2>
+      <p class="lead">Octobre 2014, l’Assemblé Nationale l’adoption du projet de loi relatif à la transition énergétique pour une croissance verte (314 voix pour et 219 contre). Le texte de loi contient une série d’articles qui vise le renforcement de l’indépendance énergétique de la France, la lutte contre les gaspillages, la promotion de l’économie circulaire et la lutte contre le changement climatique.
+Dans son article 22 ter A, le texte de loi défini l’obsolescence programmée comme :
+     </p>
+    
+      <div class=row>
+        
+    
+        <div class="col-md-8">
+          <p class="lead">Dans son article 22 ter A, le texte de loi défini l’obsolescence programmée comme :<br>
+
+<i>« I. – <i>L’ensemble</i> des techniques par lesquelles un metteur sur le marché vise,notamment par la conception du produit, à raccourcir délibérément la durée de vie ou d’utilisation potentielle de ce produit afin d’en augmenter le taux de remplacement.<i><br>
+       	</p>
+        </div>
+
+	<div class="col-md-4">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" 			src="assets/assemblee.jpg">
+ 	</div>
+	</div>
+	<p class="lead">
+<i>II. – Ces techniques peuvent notamment inclure l’introduction volontaire d’une défectuosité, d’une fragilité, d’un arrêt programmé ou prématuré, d’une limitation technique, d’une impossibilité de réparer ou d’une non-compatibilité. »</i><a href=http://www.developpement-durable.gouv.fr/IMG/pdf/0-_Projet_de_loi_relatif_a_la_transition_energetique_pour_la_croissance_verte.pdf>Voir le texte de la loi</a>
+      </p>
+      
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+           L’obsolescence programmée des produits pourra être punie, l’amendement comprend également des sanctions pour ceux qui ne respecteraient pas ces engagements. Le texte prévoit une amende maximale allant jusqu’à 300 000 euros et une peine de prison de deux ans au plus. L’affichage de la durée de vie des produits deviendra obligatoire à partir d’une valeur équivalente à 30 % du Smic.
+<br>
+Une partie des politiques est donc consciente de la problématique de l’obsolescence programmée et cherchent eux aussi des solutions viables. D’autres idées semblent également envisageables par les villes ou communautés.
+        </div>
+      </div>
+    
+    </div><!-- /.container -->
+
+
+<?php  include("footer.php");       ?>
