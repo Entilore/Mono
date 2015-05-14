8e580aae226974cc9601f48120b93a4059b434ae
masoumiaa <aminesoumiaa@gmail.com>
2015-05-14 16:24:21 +0200

    page réparation

---
+++ Website/réparation.php
@@ -0,0 +1,48 @@
+<?php  include("header_navbar.php");       ?>
+
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container">
+      <h2>Les associations et sites de réparation : </h2>
+      <p class="lead">Le Centre d'information indépendante sur les déchets et Les Amis de la terre sont parmi 
+     </p>
+    
+      <div class=row>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/amisdelaterre.jpg">
+              <figcaption>Les amis de la terre</figcaption>
+        </div>
+    
+        <div class="col-md-9">
+          <p class="lead">gggggggggggggggggggggg
+           </p>
+	<div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/cniid.jpg">
+              <figcaption>CNIID</figcaption>
+        </div>
+        </div>
+      </div>
+      
+      <div class=row>
+        <div class="col-md-9">
+          <p class="lead">entreprise de réparation
+          </p>
+        </div>
+        <div class="col-md-3">
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/repartout.jpg">
+              <figcaption><strong>Entreprise Répar'Tout</strong></figcaption>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead">
+           blablabala
+          </p>
+        </div>
+      </div>
+    
+    </div><!-- /.container -->
+
+
+<?php  include("footer.php");       ?>
