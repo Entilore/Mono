17834e11d81ce85b5a52cfed8597643e8a5dab02
masoumiaa <aminesoumiaa@gmail.com>
2015-05-14 12:59:42 +0200

    modif page :-à propos-

--- Website/a-propos.php
+++ Website/a-propos.php
@@ -3,13 +3,25 @@
     <!-- Wrap the rest of the page in another container to center all the content. -->
 
     <div class="container">
-    <h2>À propos</h2>
-    <p>
+    
+	<h1>À propos du site</h1><br>
+	<h3>L'obsolescence Programmée </h3>
+	<p>Projet de Sciences Humaines de 3ème année à l’INSA de Rennes. </p>
 
-    </p>
-    <h3>Mentions légales</h3>
-    <p>
-    </p>
+	<h3>Membres </h3>
+	<p>Jean CHORIN, Thomas HAREAU, Maximilien RICHER, Olivier DE JONCKERE, M’hamed-Amine SOUMIAA  </p>
+
+ 	<h3>Webmaster </h3>
+	<p>Maximilien RICHER  </p>
+
+	<h3>Adresse administrative</h3>
+	<p>INSA de Rennes </p>
+	<p>20, avenue des Buttes de Coesmes </p>
+	<p>CS 70839 </p>
+	<p>Rennes Cedex 7  </p>
+    
+	<h3>Hébergeur </h3>
+	<p>Centre des Ressources Informatiques de l’INSA de Rennes </p>
     
     </div><!-- /.container -->
 
