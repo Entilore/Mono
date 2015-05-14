22c5f2de4edb59fcb9f9afb83f6a4716789a5e60
Thomas Hareau <thomashareau@free.fr>
2015-05-14 20:40:03 +0200

    transform cartographie.html in cartographie.php

--- Website/header_navbar.php
+++ Website/header_navbar.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1,user-scalable=no" />
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
@@ -22,7 +22,25 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <?php
+    if(isset($cart0)){
+      ?>
+      
+    <!--[if IE]><script type="text/javascript" src="js/carto/excanvas.js"></script><![endif]--> <!-- js/carto/default.js -->
+    <script src="js/carto/jquery/jquery.min.js" type="text/javascript"></script>
+    <script src="js/carto/sigma/sigma.min.js" type="text/javascript" language="javascript"></script>
+    <script src="js/carto/sigma/sigma.parseJson.js" type="text/javascript" language="javascript"></script>
+    <script src="js/carto/fancybox/jquery.fancybox.pack.js" type="text/javascript" language="javascript"></script>
+    <script src="js/carto/main.js" type="text/javascript" language="javascript"></script>
+
     
+    <link rel="stylesheet" type="text/css" href="js/carto/fancybox/jquery.fancybox.css"/>
+    <link rel="stylesheet" href="css/carto.css" type="text/css" media="screen" />
+    <link rel="stylesheet" media="screen and (max-height: 770px)" href="css/tablet.css" />
+    <?php
+      }
+    ?>
   </head>
 
   <body style="background-image:url(assets/bg.png)">
