398f160310240776430547a547f5cb686c50c645
Jean Chorin <jchorin@insa-rennes.fr>
2015-05-17 13:30:58 +0200

    exemples.php modification of image size

--- Website/exemples.php
+++ Website/exemples.php
@@ -119,12 +119,12 @@
     
         <div class="col-md-9">
           <p class="lead">Avant le cartel, les industries produisaient des lampes avec une durée de vie allant  jusqu’à 2300 heures 
-            en moyenne.  Ils ont ensuite décidé que la durée de vie des ampoules ne devrait pas dépasser 1000h. D’après 
-            plusieurs chercheurs, le cartel avait pour but de limiter la durée de vie des lampes et  de pousser les gens à acheter plus 
+            en moyenne.  Ils ont ensuite décidé que la durée de vie des ampoules ne devrait pas dépasser 1000h. 
+          </p>
+          <p class="lead">
+            D’après plusieurs chercheurs, le cartel avait pour but de limiter la durée de vie des lampes et  de pousser les gens à acheter plus 
             de lampes. Il y a aussi une étude qui a montré que le filament de fer des ampoules de nos jours est plus fragile que sur les 
             anciennes ampoules. 
-
-
            </p>
         </div>
       </div>
@@ -204,18 +204,21 @@
     
         <div class="col-md-9">
           <p class="lead">Le résultat fut un succès total. Plus de 16 millions d’exemplaires vendus et une production sur 19 ans (de 1908 à 1927). La Ford T est la deuxième voiture la plus produite au monde après la Coccinelle de VolksWagen et 21 millions d’exemplaires.
-La volonté de Henry Ford était d’avoir une voiture robuste mais peu chère. Etant de la vieille école, il considérait que ses produits devaient être durables. Il permit ainsi à de nombreuses familles de la classe moyenne de posséder une voiture, chose impensable jusque là. En effet la voiture était auparavant un luxe. Celles-ci étaient fabriquée par des ouvriers spécialisés capables de la construire entièrement par eux-même. Le prix était donc plus important. Désormais, chaque ouvrier avait sa tâche spécifique, qu’il effectuait toute la journée de façon répétitive. Par exemple la tâche de fixer deux boulons sur la carosserie était assignée à une seule personne. Qu'elle effectuait toute la journée.
+La volonté de Henry Ford était d’avoir une voiture robuste mais peu chère. Etant de la vieille école, il considérait que ses produits devaient être durables. Il permit ainsi à de nombreuses familles de la classe moyenne de posséder une voiture, chose impensable jusque là. 
+
 
 
            </p>
         </div>
       </div>
-      
+      <br/><br/>
       <div class=row>
         <div class="col-md-9">
-          <p class="lead">Devant le succès de la Ford T, Alfred Sloan, alors directeur de General Motors, décida de tenter une autre approche. Plutôt que de produire la même voiture, il a lancé de nouveaux modèles périodiquement de façon à pousser les consommateurs à l’achat. En 1923 le premier sortit des usines. Les voitures, moins robustes, tombèrent souvent en panne après quelques années. De façon à laisser les acheteurs le temps de rembourser leur crédit.  Après avoir tenté vainement de garder l’idée de produits robustes, le succès de Sloan obliga Henry Ford à adopter sa méthode. La marque décida donc de se lancer dans la production d’autres voitures, comme le modèle A.
-
-
+          <p class="lead">
+            En effet la voiture était auparavant un luxe. Celles-ci étaient fabriquée par des ouvriers spécialisés capables de la construire entièrement par eux-même. Le prix était donc plus important. Désormais, chaque ouvrier avait sa tâche spécifique, qu’il effectuait toute la journée de façon répétitive. Par exemple la tâche de fixer deux boulons sur la carosserie était assignée à une seule personne. Qu'elle effectuait toute la journée.
+          </p>
+          <p class="lead">
+            Devant le succès de la Ford T, Alfred Sloan, alors directeur de General Motors, décida de tenter une autre approche. Plutôt que de produire la même voiture, il a lancé de nouveaux modèles périodiquement de façon à pousser les consommateurs à l’achat. En 1923 le premier sortit des usines. Les voitures, moins robustes, tombèrent souvent en panne après quelques années. De façon à laisser les acheteurs le temps de rembourser leur crédit.  Après avoir tenté vainement de garder l’idée de produits robustes, le succès de Sloan obliga Henry Ford à adopter sa méthode. La marque décida donc de se lancer dans la production d’autres voitures, comme le modèle A.
           </p>
         </div>
         <div class="col-md-3">
