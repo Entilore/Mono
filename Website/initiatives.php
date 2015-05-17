9baac9ec37ff92061ea696fa5dfd30a733d4d046
Jean Chorin <jchorin@insa-rennes.fr>
2015-05-17 13:29:43 +0200

    initiatives.php modification for image size

--- Website/initiatives.php
+++ Website/initiatives.php
@@ -12,13 +12,15 @@
       <div class=row>
         <div class="col-md-3">
               <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/pb.jpg">
-              <figcaption>Phonebloks</figcaption>
+              <figcaption> Le concept du  Phonebloks</figcaption>
         </div>
     
         <div class="col-md-9">
-          <p class="lead">Crée en 2013 sur une idée du designer Néerlandais Dave Hakkens, le <i>Phonebloks</i> est un projet de téléphone modulaire. Le smartphone est consitué d’une plaque sur laquelle on peut fixer un écran tactile et des <i>bloks</i>. Caméra, mémoire, batterie, tous les bloks ont une fonction spécifique et sont remplaçable facilement. Le but était de pouvoir changer un composant défectueux sans devoir remplacer entièrement l’appareil. Cela permettrait de réduire les couts d’achats, mais aussi les déchets !
-
-
+          <p class="lead">
+            Crée en 2013 sur une idée du designer Néerlandais Dave Hakkens, le <i>Phonebloks</i> est un projet de téléphone modulaire. Le smartphone est consitué d’une plaque sur laquelle on peut fixer un écran tactile et des <i>bloks</i>. Caméra, mémoire, batterie, tous les bloks ont une fonction spécifique et sont remplaçable facilement. 
+          </p>
+          <p class="lead">
+            Le but était de pouvoir changer un composant défectueux sans devoir remplacer entièrement l’appareil. Cela permettrait de réduire les couts d’achats, mais aussi les déchets !
            </p>
         </div>
       </div>
@@ -31,7 +33,7 @@
         </div>
         <div class="col-md-3">
               <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/ara2.jpg">
-              <figcaption>Ara Phone</figcaption>
+              <figcaption>Un exemple d'Ara Phone</figcaption>
         </div>
       </div>
     
@@ -41,23 +43,30 @@
 
       <h2 id="ara">Les associations et sites de réparation : </h2>
     
+      <p class="lead">
+        Quand un bien électrique que nous possèdons nous lâche, la plupart du temps on le jette. En effet la réparation coûte parfois plus cher que le prix d’achat Ou la pièce nécessaire n’est plus disponible. On abandonne donc et on jette nos produits. 
+      </p>
       <div class=row>
         <div class="col-md-3">
-              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/Ford-T.jpg">
-              <figcaption><strong>IMAGE A CHANGER</strong></figcaption>
+              <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/commentreparer.png">
+              <figcaption>Le site commentreparer.com</figcaption>
         </div>
     
         <div class="col-md-9">
-          <p class="lead">Lorem Ipsum
-
-
-           </p>
+          <p class="lead">
+            Pour lutter contre cet état de fait, certains ont eu une idée : pourquoi les consommateurs ne répareraient pas eux-mêmes ? C’est dans cette optique que le site <a href=”commentreparer.com” target=”_blank”>commentreparer.com</a> a été crée. Il met en lien des personne ayant un appareil défectueux et des personnes expliquant comment les réparer.
+          </p>
+          <p class="lead">
+            D’autres sites comme <a href=”www.ifixit.com” target=”_blank”>ifixit.com</a> proposent également des guides de réparation, guides d’achats et autres conseils. Ce dernier donne également une note à différents produits en fonction de leur réparabilité : l’iPhone  a ainsi un  score de 7 sur 10 tandis que le HTC One obtient 2 sur 10.
+          </p>
         </div>
       </div>
       
       <div class=row>
         <div class="col-md-9">
-          <p class="lead">Lorem Ipsum
+          <p class="lead">
+
+          </p>
         </div>
         <div class="col-md-3">
               <img class="featurette-image img-responsive img-thumbnail" data-src="holder.js/500x500/auto" alt="Generic placeholder image" src="assets/guiyu.jpg">
